e3c0d50c95032244aae96cc13a4b044cb0dccaaa
DAVENJAGI <njagidave39@gmail.com>
2023-02-16 10:02:20 -0800

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include<stdio.h>
 /**
- * main - A program that prints the size of various types inn the computer
- * Return:0(success)
+ * main - A program that prints the size of various types in the computer
+ * Return: 0(success)
  */
 
 int main(void)
