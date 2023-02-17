904a9256bddc892caf61af00aa1aa037c4f9e021
DAVENJAGI <njagidave39@gmail.com>
2023-02-17 12:26:18 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - Program that prints alphabets in lower case
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
