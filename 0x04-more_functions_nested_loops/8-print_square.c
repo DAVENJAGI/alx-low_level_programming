3aba008b6773e88f66f65752df584ed7d9a0b401
DAVENJAGI <njagidave39@gmail.com>
2023-02-23 11:30:10 -0800

    Task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,18 +3,29 @@
 #include "unistd.h"
 
 /**
- * print_square -prints a square, followed by a new line
- * @size: Size of the square
- * Return: 0 Always
+ * print_square -  prints n squares according n number of times
+ * @size: the number of squares /number of times
+ * Return: empty
  */
 
 void print_square(int size)
 {
-	int i;
 
-	for (i = 0; i < size; i++)
-	{
-	putchar(35);
-	}
-	putchar('\n');
+int x, y;
+
+if (size <= 0)
+{
+putchar('\n');
+}
+else
+{
+for (x = 0; x < size; x++)
+{
+for (y = 0; y < size; y++)
+{
+putchar(35);
+}
+putchar('\n');
+}
+}
 }
