a5732aac50b0ebc3b03e24e4b5281d3a626538b3
DAVENJAGI <njagidave39@gmail.com>
2023-03-30 08:21:59 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,20 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t i;
+	size_t i = 0;
 
-	for (i = 0; h != NULL; i++)
-
+	while (h)
 	{
 
 		if (h->str == NULL)
-		{
 			printf("[0] (nil)\n");
-		}
+
 		else
-		{
 			printf("[%u] %s\n", h->len, h->str);
-		}
+		
+		i++;
 		h = h->next;
 	}
 	return (i);
