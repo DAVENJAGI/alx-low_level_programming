6d70d8d18251a13fa40c1c5bf78898709c4868f4
DAVENJAGI <njagidave39@gmail.com>
2023-04-10 06:15:29 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,7 +16,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	if (!text_content)
-		text_content = ('\0');
+		text_content = "";
 
 	U = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 00600);
 	if (U < 0)
