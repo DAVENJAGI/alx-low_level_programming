410cd1e0e152e2153957a8ce793c4dd0880c0ea0
DAVENJAGI <njagidave39@gmail.com>
2023-12-13 17:19:22 -0500

    Search_Algorithms

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,9 @@
+#define SEARCH_ALGOS_H
+#ifndef SEARCH_ALGOS_H
+
+/* Prototypes*/
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
